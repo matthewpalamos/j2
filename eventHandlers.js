82189aef8d286edd19a2acda08f2ec55a157c2f8
Matt Palamos <mattpalamos@hotmail.com>
2017-12-30 16:32:05 -0800

    added onclick handlers to scroll to different locations

---
+++ eventHandlers.js
@@ -0,0 +1,14 @@
+
+
+$(".portfolio").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".portfolioPage").offset().top},
+        'slow');
+});
+
+
+$(".dropdown-menu").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".services").offset().top},
+        'slow');
+});
